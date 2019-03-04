ff12174c9dc7bc692966e9622a4bd19b23d16c98
vlevin <vlevin@msn.cn>
2019-03-04 21:41:50 +0800

    删除部分没有证书的奖项

--- src/components/entries/award.js
+++ src/components/entries/award.js
@@ -39,24 +39,9 @@ const idea = [
     prize: '校级二等奖',
     collapse: true
   }, {
-    time: '2017.04',
-    awardName: '高新区玄马基地创业比赛',
-    prize: '市级三等奖',
-    collapse: true
-  }, {
     time: '2016.12',
     awardName: '红牛五十元创业大赛',
     prize: '校级一等奖',
-    collapse: true
-  }, {
-    time: '2016.06',
-    awardName: '第二届互联网+',
-    prize: '校级二等奖',
-    collapse: true
-  }, {
-    time: '2016.06',
-    awardName: '海尔创青春',
-    prize: '市级创新奖',
     collapse: true
   }, {
     time: '2016.05',
@@ -68,11 +53,15 @@ const idea = [
 const other = [
   {
     time: '2018.09',
-    detail: '中国海洋大学社会实践奖学金',
+    detail: '2017-2018中国海洋大学社会实践奖学金',
     collapse: false
   }, {
     time: '2018.07',
     detail: '第九届联想idea精英汇鬼斧神工奖（全国共6名）',
+    collapse: false
+  }, {
+    time: '2017.11',
+    detail: '外国语学院自强社“榜样的力量”创业达人',
     collapse: false
   }, {
     time: '2016.09',
@@ -80,7 +69,7 @@ const other = [
     collapse: true
   }, {
     time: '2016.09',
-    detail: '中国海洋大学社会实践奖学金',
+    detail: '2015-2016中国海洋大学社会实践奖学金',
     collapse: true
   }, {
     time: '2016.09',
